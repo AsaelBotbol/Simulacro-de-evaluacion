6bc5e30d9563e6346d557bf11ad3a4937e1cdb9b
Asael Botbol <101400228+AsaelBotbol@users.noreply.github.com>
2022-04-07 15:09:03 -0300

    Ejs 1 y 2

---
+++ Assets/Scripts/Ej2.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Ej2 : MonoBehaviour
+{
+    public int prod1;
+    public int prod2;
+    public int prod3;
+    public int dinerodisp;
+    // Start is called before the first frame update
+    void Start()
+    {
+        int suma = prod1 + prod2 + prod3;
+        if(suma > dinerodisp)
+        {
+            int falta = suma - dinerodisp;
+            Debug.Log("No tenes el dinero suficiente, te faltan $" + falta);
+
+        }else if(suma < dinerodisp)
+        {
+            int sobra = dinerodisp - suma;
+            Debug.Log("Si, puedes comprar los productos. Te sobran $" + sobra);
+        }
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
